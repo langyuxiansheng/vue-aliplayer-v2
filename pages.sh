3593da7f8e4c204dc8119ede98dedf64653702c1
zhouliujun <1096432931@qq.com>
2021-06-21 17:11:13 +0800

    更新pages

---
+++ pages.sh
@@ -0,0 +1,25 @@
+echo "打包 => dist";
+
+# 打包 => dist
+npm run build;
+
+# 更新gh-pages
+echo "更新gh-pages";
+
+git branch -D gh-pages;
+
+echo "删除gh-pages";
+
+git checkout -b gh-pages;
+
+echo "创建并切换到gh-pages";
+
+git add -f dist;
+
+git commit -m 'create gh-pages';
+
+git push origin -d gh-pages;
+
+git subtree push --prefix dist origin gh-pages;
+
+echo "已推送gh-pages";
